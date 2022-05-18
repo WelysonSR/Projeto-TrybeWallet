0f7f3db4ab58d3207fce0f99b1039cc109ed4b8b
Welyson <welysonsr@gmail.com>
2022-05-18 18:30:53 -0300

    Configurando o Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
-// Coloque aqui suas actions
+const userAction = (state) => ({ type: 'NEW_USER', state });
+const walletAction = (state) => ({ type: 'NEW_WALLTMOEDA', state });
+const walletSave = (state) => ({ type: 'NEW_WALLTSAVE', state });
+
+export { userAction, walletAction, walletSave };
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'NEW_WALLTMOEDA':
+    return { ...state, currencies: action.state };
+  case 'NEW_WALLTSAVE':
+    return { ...state, expenses: [...state.expenses, action.state] };
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
