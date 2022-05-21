394aad6802ead386b10d39462025ce58bcffea97
Welyson <welysonsr@gmail.com>
2022-05-21 10:23:22 -0300

    9º Requsito concluido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { walletSum } from '../actions';
 
 class Header extends React.Component {
+  componentDidMount() {
+    const { expenses, sumWallet } = this.props;
+    let total = 0;
+    expenses.forEach((expense) => {
+      const { value, currency, exchangeRates } = expense;
+      const { ask } = exchangeRates[currency];
+      total += Number(value) * Number(ask);
+    });
+    sumWallet(total);
+  }
+
   print = () => {
     const { expenses, getSum } = this.props;
     return expenses.length === 0 ? 0 : getSum;
@@ -33,10 +45,14 @@ const mapStateToProps = (state) => ({
   getSum: state.wallet.sum,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  sumWallet: (state) => dispatch(walletSum(state)),
+});
+
 Header.propTypes = {
   emailUser: PropTypes.string,
   getSum: PropTypes.number,
   expenses: PropTypes.array,
 }.isRequired;
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
